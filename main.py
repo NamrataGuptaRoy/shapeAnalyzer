690aa8a5753a7f7aa77477ee830f9e97589a79f0
Pinaki Nath Chowdhury <pinakinathc@gmail.com>
2018-03-31 00:15:01 +0530

    Fixed error in main.py

--- main.py
+++ main.py
@@ -12,21 +12,22 @@ try:
     print sys.argv[1]
     filenames = glob.glob(sys.argv[1]+"/*.fits")
     print filenames
-    sim = []
-    for filename in filenames:
-        print "For File: ", filename.split('/')[1]
-        sim1 = similarity_Hu(filter_image(read_image(filename)))
-        sim2 = similarity_Hausdorff(filter_image(read_image(filename)))
-    for i in sim1:
-        for j in i:
-            print j,'||\t',
-        print
-    show_plot(sim1, filenames, False, "Hu_moments")
-    for i in sim2:
-        for j in i:
-            print j,'||\t',
-        print
-    show_plot(sim2, filenames, True, "Hausdorff Distance")
 except:
     print "Correct Way of execution: python main.py <input_directory>"
+    exit(1)
 
+sim = []
+for filename in filenames:
+    print "For File: ", filename.split('/')[1]
+    sim1 = similarity_Hu(filter_image(read_image(filename)))
+    sim2 = similarity_Hausdorff(filter_image(read_image(filename)))
+for i in sim1:
+    for j in i:
+        print j,'||\t',
+    print
+show_plot(sim1, filenames, False, "Hu_moments")
+for i in sim2:
+    for j in i:
+        print j,'||\t',
+    print
+show_plot(sim2, filenames, True, "Hausdorff Distance")
